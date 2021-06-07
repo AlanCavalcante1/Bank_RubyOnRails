528931effb77af7cb671b152a7dd1e4721dc7c06
Alan Cavalcante <alanlimaemail@gmail.com>
2021-06-07 02:04:35 -0300

    add BankAccount

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_one :bank_account
+
   validates :first_name, :last_name, presence: true
-  VALIDATE_EMAIL_FORMAT = /\A([^@\s]+)@([a-z0-9\.-]+)\.([a-z]{2,8})(\.[a-z]{2})?\Z/i
-  #VALIDATE_EMAIL_FORMAT = /\A([a-z0-9\.-_]+)@( (?:[-a-z0-9]+\.) +[a-z]{2,} )\Z/i
+  VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALIDATE_EMAIL_FORMAT, message: "email_format_incorrect" }
   VALIDATE_CPF_FORMAT = /\A[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}\Z/
   validates :cpf, presence: true, uniqueness: true, format: {with: VALIDATE_CPF_FORMAT, message: "cpf_format_incorrect"}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_191531) do
+ActiveRecord::Schema.define(version: 2021_06_07_045731) do
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.float "balance", default: 0.0
+    t.string "account_number"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bank_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
