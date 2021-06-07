5c8834f3fca076e805d6bbeb96541026b7f49370
Alan Cavalcante <alanlimaemail@gmail.com>
2021-06-07 14:12:43 -0300

    fix error validate_email_format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :bank_account
 
   validates :first_name, :last_name, presence: true
-  VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  VALIDATE_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALIDATE_EMAIL_FORMAT, message: "email_format_incorrect" }
   VALIDATE_CPF_FORMAT = /\A[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}\Z/
   validates :cpf, presence: true, uniqueness: true, format: {with: VALIDATE_CPF_FORMAT, message: "cpf_format_incorrect"}
