7f0e19346807b60629bf5455d6fd9b857440b0d3
Alan Cavalcante <alanlimaemail@gmail.com>
2021-06-07 14:19:19 -0300

    create user-post

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'factories/user'
 
 RSpec.describe "Users", type: :request do
 
@@ -9,5 +10,20 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
 
+  end
+
+  describe "POST /users" do
+    it "create - JSON" do
+      user = create(:user)
+      users_params = attributes_for(:user)
+      post "/users", params: {user: users_params}
+      expect(response.body).to include_json(
+        id: /\d/,
+        first_name: users_params[:first_name],
+        last_name: users_params[:last_name],
+        email: users_params[:email],
+        cpf: users_params[:cpf]
+      )
+    end
   end
 end
