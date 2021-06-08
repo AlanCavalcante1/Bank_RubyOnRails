993aeaccb66fa9f9ac55206f8d8b2027862bc377
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-08 17:59:20 -0300

    create routes to sign_up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/login', to: 'authentication#login'
+  post '/sign_up', to: 'authentication#sign_up'
   resources :bank_accounts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
