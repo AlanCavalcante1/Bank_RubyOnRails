4fcbdb3bf9c15dc60f71bc9ffc8b712617501c92
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-09 19:43:59 -0300

    create AuthenticationController

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,15 @@
 class AuthenticationController < ApplicationController
   def login
+    
+    user = User.find_by(email: params[:user][:email])
+    user = user&.authenticate(params[:user][:password])
+    
+    if user
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: {token: token, user: user}
+    else
+      render json: {message: "Nao foi possivel fazer o login"}, status: 401
+    end
   end
 
   def sign_up
